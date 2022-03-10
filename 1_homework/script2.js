48a6a23655b62bead22fcfe1daae2683f030c8d5
naijel <naijel.a@gmail.com>
2022-03-10 15:53:51 +0200

    feat: rework implementation with another conditions

--- 1_homework/script2.js
+++ 1_homework/script2.js
@@ -1,17 +1,11 @@
 const ENTER_DATA_1 = +prompt('Enter first number');
+const ENTER_DATA_2 = +prompt('Enter second number');
 
-if (isNaN(ENTER_DATA_1)) {
+if (isNaN(ENTER_DATA_1) || isNaN(ENTER_DATA_2)) {
   console.log('Некорректный ввод!');
 } else {
-  const ENTER_DATA_2 = +prompt('Enter second number');
+  const summary = ENTER_DATA_1 + ENTER_DATA_2;
+  const divisor = ENTER_DATA_1 / ENTER_DATA_2;
 
-  if (isNaN(ENTER_DATA_2)) {
-    console.log('Некорректный ввод!');
-  } else {
-    const summary = ENTER_DATA_1 + ENTER_DATA_2;
-    const divisor = ENTER_DATA_1 / ENTER_DATA_2;
-
-    console.log(`Ответ: ${summary}, ${divisor}.`);
-  }
-    
+  console.log(`Ответ: ${summary}, ${divisor}.`);
 }
